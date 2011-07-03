3cfac712181528ea464fdcd0f9a604ae68128cee
Arsyan Ismail <arsyan@arsyan.com>
2011-07-03 15:22:50 +0800

    Quick Push

---
+++ perspective/header.php
@@ -0,0 +1,99 @@
+<style>
+body
+{
+    margin:0;
+    font-family: "century gothic",arial,tahoma;
+    color: #666666;
+}
+#header
+{
+    background-image: -webkit-gradient(
+    linear,
+    left bottom,
+    left top,
+    color-stop(0.13, rgb(76,78,114)),
+    color-stop(0.57, rgb(144,151,194))
+    );
+    background-image: -moz-linear-gradient(
+    center bottom,
+    rgb(76,78,114) 13%,
+    rgb(144,151,194) 57%
+    );
+}
+
+#footer
+{
+    background-image: -webkit-gradient(
+    linear,
+    left bottom,
+    left top,
+    color-stop(0.13, rgb(76,78,114)),
+    color-stop(0.57, rgb(144,151,194))
+    );
+    background-image: -moz-linear-gradient(
+    center bottom,
+    rgb(76,78,114) 13%,
+    rgb(144,151,194) 57%
+    );
+    height:50px;
+}
+
+.tabs
+{
+    float:right;
+    background:#efefef;
+    margin:10 10 0 0;
+    padding:10px;
+    width:100px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    text-align: center;
+
+    background-image: -webkit-gradient(
+    linear,
+    left bottom,
+    left top,
+    color-stop(0.13, rgb(160,160,163)),
+    color-stop(0.57, rgb(235,232,235))
+    );
+    background-image: -moz-linear-gradient(
+    center bottom,
+    rgb(160,160,163) 13%,
+    rgb(235,232,235) 57%
+    );
+    
+}
+
+.tabs_on
+{
+    float:right;
+    background:#efefef;
+    margin:10 10 0 0;
+    padding:10px;
+    width:100px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    text-align: center;
+    color:white;
+
+    background-image: -webkit-gradient(
+    linear,
+    left bottom,
+    left top,
+    color-stop(0.18, rgb(15,15,15)),
+    color-stop(0.59, rgb(168,168,168))
+    );
+    background-image: -moz-linear-gradient(
+    center bottom,
+    rgb(15,15,15) 18%,
+    rgb(168,168,168) 59%
+    );
+}
+</style>
+
+<div id="header">
+    <img src="img/header_perspective.png">
+    <div class="tabs">Videos</div>
+    <div class="tabs">Twitter</div>
+    <div class="tabs_on">Essays</div>
+</div>
---
+++ perspective/index.php
@@ -0,0 +1,35 @@
+<?php require_once "header.php" ?>
+<table width="100%" cellpadding="0" cellspacing="0">
+    <td width="70%">
+        <div style="padding:20">
+        <span style="font-size:20px;font-weight:bold;">Lorem Ipsum is simply dummy text of the printing</span><br/>
+        <span style="font-size:12px;">Published Date: 3rd July 2011</span><br/><br/>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <br/><br/>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        <br/><br/>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+        <br/><br/>
+        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+        <br/><br/>
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+        </div>
+    </td>
+
+    <td width="30%" bgcolor="#efefef" valign="top">
+        <div style="padding:20">
+            <table width="100%">
+                <td width="130" valign="top"><img src="img/obama.png" height="150" border="1">                </td>
+                <td valign="top" style="font-size:14px">
+                    <b>Author:</b><br/>
+                    Barack Obama
+                    <br/><br/>
+                    <b>Background:</b>
+                    Barack Hussein Obama II  born August 4, 1961) is the 44th and current President of the United States. He is the first African American to hold the office. Obama previously served as a United States senator from Illinois, from January 2005 until he resigned following his victory in the 2008 presidential election.<br/><br/>
+                </td>
+            </table>
+        </div>
+        <hr/>
+    </td>
+</table>
+<?php require_once "footer.php" ?>
