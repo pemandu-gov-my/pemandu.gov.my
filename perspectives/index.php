ba70cbc6d40ab1a092992284f764652ef8df7f89
Arsyan <arsyan@arsyan.com>
2011-07-20 16:06:04 +0800

    Added text change

--- perspectives/index.php
+++ perspectives/index.php
@@ -6,26 +6,32 @@
 <h1>What is it?</h1>
 <table width="100%" cellpadding="0" cellspacing="0">
     <td height="330px" width="265" valign="top" style="background:url('img/speechBubble.png');background-repeat: no-repeat;">
-        <div style="width:220;margin:20px 0 0 25px">
-        This column serves to <b>highlight heartfelt messages from Malaysians</b> across all walks of life based on
-        what they wish for the country to be. The collection of messages is by Malaysians who have not given up
-        hope for the country and continue to strive for a better tomorrow.
+        <div style="width:220;margin:20px 0 0 25px;font-size:small;">
+        PERSPECTIVES is an online column that provides you with the opportunity to write and be heard. <br><br>
+            We would like to know anything and everything YOU Malaysians' feel about this lovely country. We want to hear it all as we are just like you and wish to preserve all that is wonderful about our culture and life, AND to erase or improve things that simply makes being Malaysian hard.
         </div>
     </td>
     <td width="20">&nbsp;</td>
     <td>
-        The characteristics of the contributors cut across individuals who:
-        <br/>
+        Are you living abroad? Tell us how you are doing, what you miss, what you don’t miss and if you ever wish to come back. Are you living here in Malaysia? Tell us those quirky only-in-Malaysia that you both love and hate.
+        <br/><br/>
+        To cut to the chase, we would like to hear the thoughts of all individuals who:
         <ul>
-        <li>Have been working abroad and desire to return to Malaysia
         <li>Want to contribute to the overall social and infrastructure health of the country
         <li>Seek to make changes in either the administrative, governance or policies of the country through positive affirmative action
-        <li>Provide constructive feedback, recommendations, suggestions or examples that may improve the country's governance further, so as to ensure Malaysia stays relevant to the bright and dedicated populace
-        <li>Ability to see the positives of being a Malaysian amidst the political noise which is sometimes irrelevant to our lives
+        <li>Provide constructive feedback, suggestions or examples that may improve the country's governance further to ensure Malaysia stays relevant to the bright and dedicated populace
         <li>Simply want to see difference in the country
         </ul>
     </td>
 </table>
+
+<br/>
+<div align="center" style="border:1px solid #333333;background:#efefef;padding:20;">
+PERSPECTIVES aims to highlight messages by Malaysians who continue to strive for a better Malaysia tomorrow.
+<br><br>
+Remember! Personal + Constructive = PERSPECTIVES.
+</div>
+
 <h1>How to Contribute?</h1>
 <img src="img/pencil.png" align="right">Prepare a write up in a 1.5 A4 page in 12 point size would be more than sufficient. The idea here is to
 draw on the positives (as well as frustrations to a degree) that all of you see, sentiments and emotions
