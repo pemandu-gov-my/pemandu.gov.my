27fe20d5809ed07bd6ec68aacd95b253708f2e39
Arsyan Ismail <arsyan@arsyan.com>
2011-08-07 01:23:34 +0800

    Fixed width iframe

--- perspectives/wuffl.php
+++ perspectives/wuffl.php
@@ -1,3 +1,3 @@
 <?php require_once "header.php"; ?>
-<iframe style="width:100%" src="http://watufighting4lah.tumblr.com/" height="600" frameborder="0">
+<iframe style="width:700" src="http://watufighting4lah.tumblr.com/" height="600" frameborder="0" scrolling="auto">
 <?php require_once "footer.php"; ?>
