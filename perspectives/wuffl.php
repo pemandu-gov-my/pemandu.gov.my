7b42c6f5486bf4158d18f83aa600ba2afb5ac979
Arsyan Ismail <arsyan@arsyan.com>
2011-08-23 00:37:41 +0800

    Expand iframe

--- perspectives/wuffl.php
+++ perspectives/wuffl.php
@@ -5,5 +5,5 @@
         visibility: hidden;
     }
 </style>
-<iframe style="width:700" src="http://watufighting4lah.tumblr.com/" height="600" frameborder="0" scrolling="auto">
+<iframe style="width:1024" src="http://watufighting4lah.tumblr.com/" height="600" frameborder="0" scrolling="auto">
 <?php require_once "footer.php"; ?>
