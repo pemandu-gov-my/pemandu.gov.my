b854c8945a1882f699513c5af631ccca25fd1a6c
Arsyan Ismail <arsyan@arsyan.com>
2011-08-25 01:32:13 +0800

    Added 4 essay writers and touchup on layout.

--- perspectives/index.php
+++ perspectives/index.php
@@ -14,8 +14,8 @@
         ?>
         <a href="essays.php?id=<?php echo $row['id'] ?>">
         <div style="width:90px;float:left;margin:0 10px 10px 0">
-            <div align="center" style="background:white;width:90px;height:66px">
-                <img src="<?php echo "img/writers/".$row['name'].".jpg" ?>" height="66" style="border:1px solid grey;"></div><span style="font-family:'century gothic',arial;font-size:10px"><?php echo $row['title'] ?> by <?php echo $row['name'] ?>
+            <div align="center" style="background:black;width:90px;height:68px;border:1px solid black;">
+                <img src="<?php echo "img/writers/".$row['name'].".jpg" ?>" height="68"></div><span style="font-family:'century gothic',arial;font-size:10px"><?php echo $row['title'] ?> by <?php echo $row['name'] ?>
             </div>
         </div>
         </a>
