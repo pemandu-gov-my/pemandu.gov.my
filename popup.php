1491d06829b1c8841ffdc0500a3a497a863ad8c6
Arsyan Ismail <arsyan@arsyan.com>
2011-10-13 20:44:19 +0800

    Popup broken link

--- popup.php
+++ popup.php
@@ -1,1 +1,4 @@
-<a target="_blank" href="http://www.youtube.com/user/gtproadmap"><img border="0" src="/img/1malaysiakedai-popup.jpg"></a>
+<style>
+    body {margin:0}
+</style>
+<a target="_blank" href="http://www.youtube.com/user/gtproadmap"><img border="0" src="/gtp/img/1malaysiakedai-popup.jpg"></a>
